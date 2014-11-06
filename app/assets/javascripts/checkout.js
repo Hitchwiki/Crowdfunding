f5284d60ded0598878d7898adc6e6a89dcf129af
Aurimas K <4urimas@gmail.com>
2014-11-06 10:31:54 +0200

    Add cardholder name and address to the payment. Make all not required fields optional (does not send together with Javascript)

--- app/assets/javascripts/checkout.js
+++ app/assets/javascripts/checkout.js
@@ -9,12 +9,27 @@ $(document).ready(function () {
       var _this = this;
       e.preventDefault();
 
-      Stripe.card.createToken({
+      var card_details = {
         number:         $('#card_number').val().replace(/ /g, ''),
         exp_month:      $('#expires_month').val(),//.split('/')[0],
         exp_year:       $('#expires_year').val(),//.split('/')[1],
-        cvc:            $('#cvv').val()
-      }, function (status, response) {
+        cvc:            $('#cvv').val(),
+        name:           $('#name').val(),
+        address_line1:    $('#address_line1').val(),
+        address_line2:    $('#address_line2').val(),
+        address_city:     $('#address_city').val(),
+        address_state:    $('#address_state').val(),
+        address_zip:      $('#address_zip').val(),
+        address_country:  $('#address_country').val()
+      }
+
+      for (var i in card_details) {
+        if (card_details[i] === null || card_details[i] === undefined || card_details[i] === '') {
+          delete card_details[i];
+        }
+      }
+
+      Stripe.card.createToken(card_details, function (status, response) {
         if (status == 200) {
           $('#stripe_token').val(response.id);
           _this.submit();
